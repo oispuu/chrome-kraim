8d226151a34dfa0144a325d213ea16f0087342a0
Brait Õispuu <oispuu@rushstreetinteractive.com>
2022-10-12 18:12:46 +0300

    Töötab aga sitalt

--- tutorials/kraim-turg/background.js
+++ tutorials/kraim-turg/background.js
@@ -1,48 +1,46 @@
 
+const kraim = 'https://punane.crime.ee';
+let state = 'OFF';
+
 chrome.runtime.onInstalled.addListener(() => {
-    console.warn('Kui tihti see litib?')
     chrome.action.setBadgeText({
-        text: "OFF",
+        text: state,
     });
 });
 
-const kraim = 'https://punane.crime.ee';
+function toggleState(tab) {
+    state = state === 'ON' ? 'OFF' : 'ON';
+    // Set the action badge to the next state
+    chrome.action.setBadgeText({
+        tabId: tab.id,
+        text: state,
+    });
+}
 
-// When the user clicks on the extension action
-chrome.action.onClicked.addListener(async (tab) => {
-    console.warn('Tõmbasime käima', tab);
-    if (tab.url.startsWith(kraim)) {
-        // We retrieve the action badge to check if the extension is 'ON' or 'OFF'
-        const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-        console.warn('Praegu badge on', prevState);
-        // Next state will always be the opposite
-        const nextState = prevState === 'ON' ? 'OFF' : 'ON';
-
-        // Set the action badge to the next state
-        await chrome.action.setBadgeText({
-            tabId: tab.id,
-            text: nextState,
-        });
-
-        if (nextState === "ON") {
-            // Insert the CSS file when the user turns the extension on
-            await chrome.scripting.executeScript({
-                files: ["scripts/turg.js"],
-                target: { tabId: tab.id },
-            });
-        }
-    }
-});
-
-chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-    const state = await chrome.action.getBadgeText({ tabId: tab.id });
-
+function execute(tab) {
+    console.warn('Litin scripti', state)
     if (state === "ON") {
         // Insert the CSS file when the user turns the extension on
-        await chrome.scripting.executeScript({
+        chrome.scripting.executeScript({
             files: ["scripts/turg.js"],
             target: { tabId: tab.id },
         });
     }
+}
+
+// When the user clicks on the extension action
+chrome.action.onClicked.addListener(async (tab) => {
+    if (tab.url.startsWith(kraim)) {
+        // We retrieve the action badge to check if the extension is 'ON' or 'OFF'
+        const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+        // Next state will always be the opposite
+        toggleState(tab);
+
+        execute(tab);
+    }
+});
+
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+    execute(tab);
 }
 );
--- tutorials/kraim-turg/scripts/turg.js
+++ tutorials/kraim-turg/scripts/turg.js
@@ -1,31 +1,35 @@
-const ahjud = 'https://punane.crime.ee/index.php?asukoht=house&tegevus=ovens';
-const rauatükid = 'asukoht=slumm&paik=4';
-const ostaSelector = 'input[name="purchcrafitem"]';
-const tostaText = 'Tõsta need nüüd seljakotist kappi';
-const kasitooTarbed = 'asukoht=house&tegevus=materialsstorage';
-const tostaSelector = 'input[name="ktookappi"]';
-const mineText = 'Mine turule';
+var ahjud = 'https://punane.crime.ee/index.php?asukoht=house&tegevus=ovens';
+var rauatükid = 'asukoht=slumm&paik=4';
+var ostaSelector = 'input[name="purchcrafitem"]';
+var tostaText = 'Tõsta need nüüd seljakotist kappi';
+var kasitooTarbed = 'asukoht=house&tegevus=materialsstorage';
+var tostaSelector = 'input[name="ktookappi"]';
+var mineText = 'Mine turule';
 
-if (window.location.href === ahjud) {
-    console.warn('Lähme turule');
-} else if (window.location.href.includes(rauatükid)) {
-    var xpath = "//a[contains(text(),'" + tostaText + "')]";
-    var matchingElement = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-
-    if (matchingElement) {
-        console.warn('Tostame');
-    } else {
-        ostaBtn = document.querySelectorAll(ostaSelector)[0];
-        console.warn('Triggerin', ostaBtn);
-    }
-} else if (window.location.href.includes(kasitooTarbed)) {
-    var xpath = "//a[contains(text(),'" + mineText + "')]";
-    var matchingElement = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-
-    if (matchingElement) {
-        console.warn('Lähme');
-    } else {
-        tostaBtn = document.querySelectorAll(tostaSelector);
-        console.warn('Triggerin', tostaBtn);
+checkYoself = () => {
+    if (window.location.href === ahjud) {
+        console.warn('Lähme turule');
+    } else if (window.location.href.includes(rauatükid)) {
+        var xpath = "//a[contains(text(),'" + tostaText + "')]";
+        var matchingElement = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    
+        if (matchingElement) {
+            matchingElement.click();
+        } else {
+            ostaBtn = document.querySelectorAll(ostaSelector)[0];
+            ostaBtn.click();
+        }
+    } else if (window.location.href.includes(kasitooTarbed)) {
+        var xpath = "//a[contains(text(),'" + mineText + "')]";
+        var matchingElement = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    
+        if (matchingElement) {
+            matchingElement.click();
+        } else {
+            tostaBtn = document.querySelectorAll(tostaSelector)[0];
+            tostaBtn.click();
+        }
     }
 }
+
+checkYoself();
